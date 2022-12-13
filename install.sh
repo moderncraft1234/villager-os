5d7cc28aa5c803f61f879dd4e73630bc229ca255
moderncraft1234 <66831184+moderncraft1234@users.noreply.github.com>
2022-12-13 11:48:14 +0100

    update on installer

--- install.sh
+++ install.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 sudo pacman -Syyu
-sudo pacman -Syy i3
-sudo pacman -Syy gdm
-sudo pacman -Syy dmenu
-sudo pacman -Syy ranger
-sudo pacman -Syy zsh
-sudo pacman -Syy networkmanager
-sudo pacman -Syy emacs
-sudo pacman -Syy nvim
-sudo pacman -Syy mc
-sudo pacman -Syy vim
-sudo pacman -Syy nvim
-sudo pacman -Syy gimp
-sudo pacman -Syy polybar
-sudo pacman -Syy nano
-sudo pacman -Syy dolphin
-sudo pacman -Syy gimp
-sudo pacman -Syy blender
-sudo pacman -Syy openssh
-sudo pacman -Syy discord
-sudo pacman -Syy konsole
-sudo pacman -Syy plasma
-sudo pacman -Syy virtualbox
-sudo pacman -S --needed git base-devel
+sudo pacman -Syy --asdeps i3
+sudo pacman -Syy --asdeps gdm
+sudo pacman -Syy --asdeps dmenu
+sudo pacman -Syy --asdeps ranger
+sudo pacman -Syy --asdeps zsh
+sudo pacman -Syy --asdeps networkmanager
+sudo pacman -Syy --asdeps emacs
+sudo pacman -Syy --asdeps nvim
+sudo pacman -Syy --asdeps mc
+sudo pacman -Syy --asdeps vim
+sudo pacman -Syy --asdeps nvim
+sudo pacman -Syy --asdeps gimp
+sudo pacman -Syy --asdeps polybar
+sudo pacman -Syy --asdeps nano
+sudo pacman -Syy --asdeps dolphin
+sudo pacman -Syy --asdeps gimp
+sudo pacman -Syy --asdeps blender
+sudo pacman -Syy --asdeps openssh
+sudo pacman -Syy --asdeps discord
+sudo pacman -Syy --asdeps konsole
+sudo pacman -Syy --asdeps plasma
+sudo pacman -Syy --asdeps virtualbox
+sudo pacman -Syy --asdeps kdenlive
+sudo pacman -S --needed --asdeps git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-cd ~/villager-os
+cd ..
 
 
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
@@ -37,18 +38,20 @@ mkdir ~/Pictures/random
 
 git clone https://github.com/moderncraft1234/villager-os/blob/main/profile ~.profile
 
-cp profile ~/.profile
+cp profile ~.profile
 
 git clone https://github.com/moderncraft1234/villager-os/tree/main/random
 
-cp /random ~/Pictures/random
+cp random ~Pictures/random
 
 yay -Sy google-chrome
 
 yay -S --noconfirm zsh-theme-powerlevel10k-git
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
-cp zshrc ~/.zshrc
+cp zshrc ~.zshrc
+
+cp p10k.zsh ~.p10k.zsh
 
 sudo systemctl enable gdm
 sudo systemctl start gdm
